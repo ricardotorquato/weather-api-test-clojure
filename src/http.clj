63e4d745407fb9321ad81e5ad367f924cc96b3aa
Ricardo Torquato <torquatto@msn.com>
2018-09-26 11:46:33 -0300

    Adding content-type to json

--- src/http.clj
+++ src/http.clj
@@ -2,7 +2,7 @@
 
 ;; Defining function response that sent status, body and headers
 (defn response [status body & {:as headers}]
-  {:status status :body body :headers headers})
+  {:status status :body body :headers (assoc headers "Content-Type" "application/json")})
 
 ;; Defining ok function as as partial of response
 (def ok (partial response 200))
